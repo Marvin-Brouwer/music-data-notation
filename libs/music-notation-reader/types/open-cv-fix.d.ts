629f53afd31ebbc71f6331da699ee89b1f2b37ca
Marvin Brouwer <marvin-brouwer@live.nl>
2025-09-27 15:59:59 +0200

    adding some eventual test images

--- libs/music-notation-reader/types/open-cv-fix.d.ts
+++
@@ -1,7 +0,0 @@
-import type * as cv from "@techstark/opencv-js";
-type _ = typeof cv;
-
-// declare module '@techstark/opencv-js' {
-//   // override Scalar typing
-//   export function Scalar(v0: number, v1?: number, v2?: number, v3?: number): number[];
-// }
